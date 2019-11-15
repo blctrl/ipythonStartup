75b5d381db214e9e627da5f8f7122a9eee310451
blctrl <zhangzhaohong@sinap.ac.cn>
2019-11-15 14:25:47 +0800

    comment the install_qt_kicker

--- 00bluesky.py
+++ 00bluesky.py
@@ -3,8 +3,8 @@ RE = RunEngine({})
 
 from bluesky.plans import count, scan
 from bluesky.callbacks import LiveTable, LivePlot
-from bluesky.utils import install_qt_kicker
-install_qt_kicker()
+#from bluesky.utils import install_qt_kicker
+#install_qt_kicker()
 
 
 
